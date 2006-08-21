38f5658cc1ba612b42c7a5e1ca4fc802aae78899
Niclas Hedhman <niclas@hedhman.org>
2006-08-21 09:50:54 +0000

    A little bit doc.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/base@4416 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- src/main/java/org/ops4j/io/StreamUtils.java
+++ src/main/java/org/ops4j/io/StreamUtils.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2004 Stephen J. McConnell.
- * Copyright 2004 Niclas Hedhman
+ * Copyright 2004, 2006 Niclas Hedhman
  * Licensed  under the  Apache License,  Version 2.0  (the "License");
  * you may not use  this file  except in  compliance with the License.
  * You may obtain a copy of the License at
